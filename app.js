97fb7af9b43d86c3725aa0ef17352d1b914b9d14
Matt Burnett <mburnett@tutanota.de>
2024-08-28 16:31:06 +0200

    Changed font to Nunito, cleanup of old app.js chia-code

--- app.js
+++ app.js
@@ -4,42 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const sectBtn = document.querySelectorAll('.control');
   const allSections = document.querySelector('.main-content');
 
-  function PageTransitions() {
-    sectBtns.forEach((btn) => {
-      btn.addEventListener('click', function(e) {
-        const clicked = e.target.closest('.control');
-        if (!clicked) return;
+  // Event listeners for the nav buttons
+  sectBtns.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+      const clicked = e.target.closest('.control');
+      if (!clicked) return;
 
-        let currentBtn = document.querySelector('.active-btn');
-        if (currentBtn) {
-          currentBtn.classList.remove('active-btn');
-        }
+      let currentBtn = document.querySelector('.active-btn');
+      if (currentBtn) {
+        currentBtn.classList.remove('active-btn');
+      }
 
-        clicked.classList.add('active-btn');
+      clicked.classList.add('active-btn');
 
-        // Additional logic for section switching
-        const id = clicked.querySelector('span').dataset.id;
-        if (id) {
-          // Remove selected from the other btns
-          sectBtns.forEach((btn) => {
-            btn.classList.remove('active');
-          });
-          e.target.classList.add('active');
-
-          // Hide other sections
-          sections.forEach((section) => {
-            section.classList.remove('active');
-          });
-
-          const element = document.getElementById(id);
-          element.classList.add('active');
-        } 
-      });
-    });
-
-    // Sections Active
-    allSections.addEventListener('click', (e) => {
-      const id = e.target.dataset.id;
+      // Additional logic for section switching
+      const id = clicked.querySelector('span').dataset.id;
       if (id) {
         // Remove selected from the other btns
         sectBtns.forEach((btn) => {
@@ -54,18 +33,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const element = document.getElementById(id);
         element.classList.add('active');
-      }
+      } 
     });
+  });
 
-    // Toggle theme
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click', () => {
-      let element = document.body;
-      element.classList.toggle('light-mode');
-    });
-  }
+  // Sections Active
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // Remove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
 
-  PageTransitions();
+      // Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
+
+  // Toggle theme
+  const themeBtn = document.querySelector('.theme-btn');
+  themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode');
+  });
 
   // Updated portfolio-item and close-dialog event listeners
   document.querySelector('.portfolio-container').addEventListener('click', function(e) {
